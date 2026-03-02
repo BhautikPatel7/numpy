f663945ae47eaede31adc07ed007916d71b1ae25
Bhautik Patel <84130806+BhautikPatel7@users.noreply.github.com>
2026-03-02 18:53:54 +0530

    workin on step 3

--- satellite_numpy_project.py
+++ satellite_numpy_project.py
@@ -16,15 +16,58 @@ import numpy as np
 # list_3d = [[[0 for _ in range(cols)] for _ in range(rows)]for _ in range(cols)]
 # print(list_3d)
 
-arr_3d = np.array([
-    [[1,2,3], [2,3,4]],
-    [[7,8,9], [8,9,11]],
-    [[7,8,9], [8,9,11]],
-    [[7,8,9], [8,9,11]]
-])
+# arr_3d = np.array([
+#     [[1,2,3], [2,3,4]],
+#     [[7,8,9], [8,9,11]],
+#     [[7,8,9], [8,9,11]],
+#     [[7,8,9], [8,9,11]]
+# ])
 
 
 
-print(arr_3d)
+# print(arr_3d)
 
-print(f"shape of array is {arr_3d.shape}")
+# print(f"shape of array is {arr_3d.shape}")
+
+Mean = 25
+
+Std = 5
+
+Shape = (365, 24, 5)
+
+
+data = np.random.normal(size=Shape, loc=Mean, scale=Std)
+# print(data.shape)
+# print(data)
+
+nan_count = 500
+outlier_count = 50
+
+indices_to_nan = np.random.choice(data.size, nan_count, replace=False)
+indicase_to_outlier = np.random.choice(data.size, outlier_count, replace=False)
+
+data.ravel()[indices_to_nan] = np.nan
+data.ravel()[indicase_to_outlier] = 999
+
+
+# print(data)
+
+
+nan_check = np.isnan(data)
+
+# print(nan_check)
+print(nan_check.shape)
+
+
+# count = np.count_nonzero(nan_check)
+
+layers, rows, cols = np.where(nan_check)
+
+print(len(layers))
+print(len(rows))
+print(len(cols))
+
+print(layers)
+print(rows)
+print(cols)
+# print(f"The number of True values is: {count}")
